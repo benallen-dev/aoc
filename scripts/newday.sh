b26be08b701d9fa036979dfb71a5ddc8216d5dbf
Ben Allen <ben@benallen.dev>
2024-09-21 12:42:01 +0200

    Add newday script to generate boilerplate ezpz

---
+++ scripts/newday.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# This script is used to create a new day's directory
+
+# Check for the correct number of arguments
+if [ $# -ne 2 ]; then
+	echo "Usage: $0 <year> <day>"
+	exit 1
+fi
+
+year=$1
+# Pad the day with a 0 if it is less than 10
+if [ $2 -lt 10 ]; then
+	day=0$2
+else
+	day=$2
+fi
+
+# if we're not in the root directory, cd to it
+pushd $(git rev-parse --show-toplevel)
+mkdir -p ./cmd/$year/$day
+cp -r ./template/* ./cmd/$year/$day
+
+touch ./cmd/$year/$day/input.txt
+touch ./cmd/$year/$day/input_example.txt
+popd
