8ce92b6b39fab21625600beb294557ae23f7e21e
Ben Allen <ben@benallen.dev>
2024-12-02 19:57:56 +0100

    Solve 2024 day 02

--- cmd/2024/02/main.go
+++ cmd/2024/02/main.go
@@ -8,13 +8,13 @@ import (
 type Direction int
 
 const (
-	DirectionDecreasing Direction = iota
-	DirectionIncreasing
+	DirUp Direction = iota
+	DirDown
 )
 
 var directionName = map[Direction]string{
-	DirectionDecreasing: "decreasing",
-	DirectionIncreasing: "increasing",
+	DirUp:   "decreasing",
+	DirDown: "increasing",
 }
 
 func (d Direction) String() string {
@@ -24,10 +24,10 @@ func (d Direction) String() string {
 func checkSafe(input []int) bool {
 	var dir Direction
 
-	if input[1] > input[0] {
-		dir = DirectionIncreasing
-	} else if input[1] < input[0] {
-		dir = DirectionDecreasing
+	if input[0] > input[1] {
+		dir = DirDown
+	} else if input[0] < input[1] {
+		dir = DirUp
 	} else {
 		return false
 	}
@@ -35,7 +35,7 @@ func checkSafe(input []int) bool {
 	for i := 1; i < len(input); i++ {
 		diff := input[i] - input[i-1]
 		mag := math.Abs(float64(diff))
-		if mag > 3 || diff == 0 || (dir == DirectionDecreasing && diff > 0) || (dir == DirectionIncreasing && diff < 0) {
+		if mag > 3 || diff == 0 || (dir == DirUp && diff < 0) || (dir == DirDown && diff > 0) {
 			return false
 		}
 	}
@@ -43,28 +43,32 @@ func checkSafe(input []int) bool {
 	return true
 }
 
+// We need to make copies to avoid modifying the input slice - the underlying array is shared
+func removeElement(input []int, index int) []int {
+	result := make([]int, len(input)-1)
+	copy(result, input[:index])
+	copy(result[index:], input[index+1:])
+
+	return result
+}
+
 func checkSafePart2(input []int) bool {
-	var dir Direction
+	// The idea behind part 2 is that one of the levels can be removed to
+	// create a valid report - this could be any of the levels
 
-	if input[1] > input[0] {
-		dir = DirectionIncreasing
-	} else if input[1] < input[0] {
-		dir = DirectionDecreasing
-	} else {
-		return false
-	}
-
-	fail := 0
-	// doesn't take into account the first pair being the wrong way around
-	for i := 1; i < len(input); i++ {
-		diff := input[i] - input[i-1]
-		mag := math.Abs(float64(diff))
-		if mag > 3 || diff == 0 || (dir == DirectionDecreasing && diff > 0) || (dir == DirectionIncreasing && diff < 0) {
-			fail++
+	// The simple implementation for this is to remove each level in turn and
+	// check if the report is valid. You could try to include this in the main
+	// loop by removing the ith element if the pair of i and i+1 are invalid
+	// and then rechecking, but you need edge cases to handle the first and
+	// last elements and this is day 2, so brute force it is.
+	for i := 0; i < len(input); i++ {
+		x := removeElement(input, i)
+		if checkSafe(x) {
+			return true
 		}
 	}
 
-	return true
+	return false
 }
 
 func main() {
@@ -73,21 +77,17 @@ func main() {
 		panic(err)
 	}
 
-	// The levels are either all increasing or all decreasing.
-	// Any two adjacent levels differ by at least one and at most three.
 	c := 0
 	d := 0
 	for _, x := range input {
 		if checkSafe(x) {
 			c++
-		}
-
-		if checkSafePart2(x) {
+			d++
+		} else if checkSafePart2(x) {
 			d++
 		}
 	}
 
 	fmt.Printf("Part 1: %d\n", c)
-
 	fmt.Printf("Part 2: %d\n", d)
 }
