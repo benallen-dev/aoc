b58b70fa2d09a429537f85711e74988d0f94a970
Ben Allen <ballen@anwb.nl>
2024-12-04 15:02:03 +0100

    Solve 2024 day 03

--- cmd/2024/03/main.go
+++ cmd/2024/03/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"regexp"
+
+	"strings"
 )
 
 func getMuls(input string) []string {
@@ -18,13 +20,8 @@ func performMul(input string) int {
 }
 
 func findDos(input string) [][]int {
-	doRegex := regexp.MustCompile(`do\(\d+\)`)
+	doRegex := regexp.MustCompile(`do\(\)`)
 	return doRegex.FindAllStringIndex(input, -1)
-}
-
-func findDonts(input string) [][]int {
-	dontRegex := regexp.MustCompile(`don't\(\)`)
-	return dontRegex.FindAllStringIndex(input, -1)
 }
 
 func main() {
@@ -39,18 +36,41 @@ func main() {
 		total += performMul(mul)
 	}
 
-	fmt.Println(total)
+	fmt.Printf("Part 1: %d\n", total)
 
-	// Ha! Serves me right for using regexes
+	// Part 2:
 	// 10 take input until "don't()", find mul()s
 	// 20 find next do() unless EOF
 	// 30 goto 10
 
-	// doIdxs := findDos(input)
-	// dontIdxs := findDonts(input)
+	// split into parts that end with "don't()"
+	parts := strings.Split(input, "don't()")
 
-	
+	var activeInput = make([]string, 0)
+	for i, part := range parts {
+		if i == 0 { // the first do() is implicit
+			activeInput = append(activeInput, part)
+			continue
+		}
+
+		// find the first do() - anything between this and \n is active
+		dos := findDos(part)
+		// Note: findDos can return nil for no match, if there's no
+		// `do()` inbetween these `don't()`s we can safely move on.
+		if len(dos) == 0 {
+			continue
+		}
 		
+		startat := dos[0][1] // include everything after 'do()'
+		activeInput = append(activeInput, part[startat:])
+	}
 
+	var total2 int
+	muls2 := getMuls(strings.Join(activeInput, ""))
+	for _, mul := range muls2 {
+		total2 += performMul(mul)
+	}
+
+	fmt.Printf("Part 2: %d\n", total2)
 }
 
